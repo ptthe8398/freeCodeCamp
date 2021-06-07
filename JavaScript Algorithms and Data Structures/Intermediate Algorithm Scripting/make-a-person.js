805126de3dc32f587e0c3d2f54c515b45b1eb19f
TPT0803 <73353299+ptthe8398@users.noreply.github.com>
2021-06-08 00:34:23 +0700

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/make-a-person.js
@@ -0,0 +1,30 @@
+var Person = function(firstAndLast) {
+  var fullName = firstAndLast;
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function(name) {
+    fullName = name;
+  };
+};
+var bob = new Person("Bob Ross");
+bob.getFullName();
